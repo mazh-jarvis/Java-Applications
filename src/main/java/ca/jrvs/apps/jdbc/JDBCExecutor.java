0117e52a8c52d9c3969e9aca69d416c92fcbee8d
mazh91 <milad.azh@ryerson.ca>
2019-06-28 10:13:55 -0400

    jdbc pre-project stuff

---
+++ src/main/java/ca/jrvs/apps/jdbc/JDBCExecutor.java
@@ -0,0 +1,37 @@
+package ca.jrvs.apps.jdbc;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class JDBCExecutor {
+    public static void main(String[] args) {
+        DatabaseConnectionManager connectionManager =
+                new DatabaseConnectionManager("localhost",
+                        "jdbc_app", "postgres", "petrop");
+        try{
+            Connection connection = connectionManager.getConnection();
+            CustomerDAO customerDAO = new CustomerDAO(connection);
+            /*Customer customer = new Customer();
+            customer.setFirstName("John");
+            customer.setLastName("Smith");
+            customer.setEmail("john.smith@fake.com");
+            customer.setPhone("(555) 432-1234");
+            customer.setAddress("123 Main St");
+            customer.setCity("York");
+            customer.setState("Eoferwic");
+            customer.setZipCode("95723");
+            customerDAO.create(customer);
+*/
+            Customer customer = customerDAO.findById(1000);
+            System.out.println(customer);
+            connection.close();
+            /*Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery("SELECT COUNT(*) FROM CUSTOMER;");*/
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
