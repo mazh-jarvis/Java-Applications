e992983af5d51c0059c801f054646a8acf7304cf
mazh91 <milad.azh@ryerson.ca>
2019-07-03 16:22:51 -0400

    make object mapper a singleton; implement toJson()

--- src/main/java/ca/jrvs/apps/twitter/dao/helper/DaoHelper.java
+++ src/main/java/ca/jrvs/apps/twitter/dao/helper/DaoHelper.java
@@ -1,6 +1,7 @@
 package ca.jrvs.apps.twitter.dao.helper;
 
 import ca.jrvs.apps.twitter.dto.Tweet;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -9,28 +10,67 @@ import java.io.InputStream;
 import java.util.stream.Stream;
 
 public class DaoHelper {
-    // Validate ID string challenge
+
+    private static ObjectMapper mapper;
+
+    private DaoHelper() {}
+
+    /**
+     * Validate ID string (challenge)
+     * @param id URL parameter (id)
+     * @return true if id is a valid input
+     */
     public static boolean validateIDStr(String id) {
         Stream<Character> charStream = id.chars().mapToObj(c -> (char)c);
         return charStream.allMatch(Character::isDigit);
     }
 
-
-
-    public static <T> T toObjectFromJson(String json, Class _class) throws IOException {
-        // json object mapper setup
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-
-        return (T) objectMapper.readValue(json, _class);
+    /**
+     * Convert a java object to JSON string
+     * @param object input object
+     * @return JSON string
+     * @throws JsonProcessingException
+     */
+    public static String toJson(Object object, boolean prettyJson, boolean includeNullValues) throws JsonProcessingException {
+         /*return getMapper().writer().withDefaultPrettyPrinter()
+                .writeValueAsString(object);*/
+       return getMapper().writeValueAsString(object);
     }
 
+    /**
+     *Parse JSON string to a object
+     * @param json JSON str
+     * @param _class object class
+     * @param <T> type
+     * @return object
+     * @throws IOException
+     */
+    public static <T> T toObjectFromJson(String json, Class _class) throws IOException {
+        return (T) getMapper().readValue(json, _class);
+    }
 
+    /**
+     * Parse JSON stream to a object
+     * @param jsonIStream JSON stream
+     * @param _class object class
+     * @param <T> type
+     * @return object
+     * @throws IOException
+     */
     public static <T> T toObjectFromJson(InputStream jsonIStream, Class _class) throws IOException {
-        // json object mapper setup
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        return (T) getMapper().readValue(jsonIStream, _class);
+    }
 
-        return (T) objectMapper.readValue(jsonIStream, _class);
+    /**
+     * Singleton object mapper
+     * initialize and configure if necessary
+     * @return singleton object mapper
+     */
+    private static ObjectMapper getMapper() {
+        if(mapper == null) {
+            mapper = new ObjectMapper();
+            mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        }
+        return mapper;
     }
 }
--- src/main/java/ca/jrvs/apps/twitter/dto/Coordinates.java
+++ src/main/java/ca/jrvs/apps/twitter/dto/Coordinates.java
@@ -1,4 +1,22 @@
 package ca.jrvs.apps.twitter.dto;
 
 public class Coordinates {
+    private float latitude,
+        longitude;
+
+    public float getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(float latitude) {
+        this.latitude = latitude;
+    }
+
+    public float getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(float longitude) {
+        this.longitude = longitude;
+    }
 }
